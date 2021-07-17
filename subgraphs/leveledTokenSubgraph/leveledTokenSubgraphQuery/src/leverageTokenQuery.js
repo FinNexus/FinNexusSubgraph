7dbbb5e6831085a0c9259cead720035a4065afba
jiaqinggang1 <forestjqg@126.com>
2021-07-17 22:21:41 +0800

    changed cal

--- subgraphs/leveledTokenSubgraph/leveledTokenSubgraphQuery/src/leverageTokenQuery.js
+++ subgraphs/leveledTokenSubgraph/leveledTokenSubgraphQuery/src/leverageTokenQuery.js
@@ -1,13 +1,13 @@
 const { createClient } = require('graphqurl');
-const BN = require("bignumber.js");
-var express = require('express');
-var app = express();
-//var Web3 = require('web3');
-//var web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/75c431806c0d49ee9868d4fdcef025bd"));
-//var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/75c431806c0d49ee9868d4fdcef025bd"));
-//var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/34b6f08707964087bea0eb7571fdae41"));
 
-//const sushuchef = new web3.eth.Contract(sushichefabi, sushichefaddress);
+const BN = require("bn.js");
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/75c431806c0d49ee9868d4fdcef025bd"));
+//var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/75c431806c0d49ee9868d4fdcef025bd"));
+
+var { abi } = require('./erc20token.json');
+var tokenabi = abi;
+
 //npm i --save lokka lokka-transport-http
 const Lokka = require('lokka').Lokka;
 const Transport = require('lokka-transport-http').Transport;
@@ -16,109 +16,261 @@ const client = new Lokka({
     transport: new Transport('https://api.thegraph.com/subgraphs/name/jeffqg123/leveragedpoolsubgraph')
 });
 
+
+const PRICE_DECIMAL = new BN("100000000");
+const EIGHTEEN_DECIMAL = new BN("1000000000000000000");
+const NETWORTH_DECIMAL = new BN("100000000");
+
+const underLying = ["", "btc", "eth", "mkr", "snx", "link"];
+const optype = ["call", "put"];
+
+function getDate(unixtime) {
+    var a = new Date(unixtime * 1000);
+    var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    date=date>=10?""+date:"0"+date;
+
+    var hour = a.getHours();
+    hour=hour>=10?""+hour:"0"+hour;
+
+    var min = a.getMinutes()
+    min=min>=10?""+min:"0"+min;
+
+    var sec = a.getSeconds();
+    sec=sec>=10?""+sec:"0"+sec;
+
+    var time = date + '-' + month + '-' + year + ' ' + hour + ':' + min + ':' + sec;
+    return time;
+};
+
 // type EntityLeveragePool @entity {
 //     id: ID!
 //         name: String
 //     underlyingAddress: Bytes
 //     underlyingName: String
 // }
-client.query(`
+// client.query(`
+//     {
+//       entityLeveragePools(first: 5) {
+//         id
+//         name
+//         underlyingAddress
+//         underlyingName
+//       }
+//     }
+// `).then(result => {
+//     console.log(result);
+// });
+
+async function getLeveragePools() {
+    let querystr = `
     {
-      entityLeveragePools(first: 5) {
+      entityLeveragePools(first: 50) {
         id
         name
         underlyingAddress
         underlyingName
-      }      
+      }
+    }`
+    let result = await client.query(querystr)
+    let leveragePools = result.entityLeveragePools;
+    //console.log(tvls)
+    let allLeveragePools = new Map();
+    for(let i=0;i<leveragePools.length;i++) {
+        let item = leveragePools[i];
+        //console.log(name,decimal)
+        let pool = {
+            Underlying: item.underlyingAddress,
+            PoolName: item.name,
+            UnderlyingName: item.underlyingName
+        }
+
+        allLeveragePools[item.id] = pool;
     }
-`).then(result => {
-    console.log(result);
-});
+    console.log(allLeveragePools);
+    return allLeveragePools;
+}
+
+//
+// getLeveragePools()
+// return;
 
 // type EntityLeverageFactory @entity {
 //     id: ID!
 // }
-client.query(`
-    {
-      entityLeverageFactories(first: 5) {
-        id
-      }      
-    }
-`).then(result => {
-    console.log("query entityLeverageFactorys")
-    console.log(result);
-});
+// client.query(`
+//     {
+//       entityLeverageFactories(first: 5) {
+//         id
+//       }
+//     }
+// `).then(result => {
+//     console.log("query entityLeverageFactorys")
+//     console.log(result);
+// });
+
 
 // type EntityTradeItem @entity {
 //     id: ID!
-//         from: Bytes!
-//         timestamp: BigInt!
-//         status: String!
-//         underlying: Bytes!
-//         leveragetype: String!
-//         value: BigInt!
-//         price: BigInt!
-//         amount: BigInt!
+//     pool: Bytes!
+//     from: Bytes!
+//     timestamp: BigInt!
+//     status: String
+//     settlement: Bytes
+//     leverageToken: Bytes
+//     leveragetype: String
+//     value: BigInt!
+//     price: BigInt!
+//     amount: BigInt!
 // }
-client.query(`
+// client.query(`
+//     {
+//       entityTradeItems(first: 5) {
+//         id
+//         pool
+//         from
+//         timestamp
+//         status
+//         settlement
+//         leverageToken
+//         leveragetype
+//         value
+//         price
+//         amount
+//       }
+//     }
+// `).then(result => {
+//     console.log("query entityTradeItems")
+//     console.log(result);
+// });
+
+async function getEntityTradeItems() {
+    let querystr = `
     {
-      entityTradeItems(first: 5) {
+      entityTradeItems(first: 1000, orderBy: timestamp, orderDirection: asc) {
         id
+        pool
         from
         timestamp
         status
-        underlying
+        settlement
+        leverageToken
         leveragetype
         value
         price
         amount
-      }      
+      }
+    }`
+    let result = await client.query(querystr)
+    let tradeItems = result.entityTradeItems;
+    console.log(tradeItems);
+
+    let pools = await getLeveragePools();
+
+    let allTradeItems = new Map();
+    for(let i=0;i<tradeItems.length;i++) {
+        let item = tradeItems[i];
+        let tk = new web3.eth.Contract(tokenabi,item.settlement);
+        let settleName = await tk.methods.symbol().call();
+
+        let tradeitem = {
+            Date: getDate(item.TimeStamp),
+            Owner: item.from,
+            Status: item.status,
+            Settlement: settleName,
+            Underlying: pools[item.pool].UnderlyingName,
+            Leveragetype: item.leveragetype,
+            Amount: new BN(item.amount).div(EIGHTEEN_DECIMAL),
+            Value: new BN(item.value).div(EIGHTEEN_DECIMAL.mul(PRICE_DECIMAL)),
+            Price: new BN(item.price).div(PRICE_DECIMAL)
+        }
+        allTradeItems[item.id].push(tradeitem);
     }
-`).then(result => {
-    console.log("query entityTradeItems")
-    console.log(result);
-});
+    //console.log(alltvls);
+    return allTradeItems
+}
+
+//getEntityTradeItems();
+//return;
+
 // type EntityTotalTVL @entity {
 //     id: ID!
 //         timestamp: BigInt!
 //         value: BigInt!
 // }
-client.query(`
-    {
-      entityTotalTVLs(first: 5) {
-        id
-        timestamp
-        value
-      }      
-    }
-`).then(result => {
-    console.log("query entityLeverageFactorys")
-    console.log(result);
-});
+
+// client.query(`
+//     {
+//       entityTotalTVLs(first: 5) {
+//         id
+//         timestamp
+//         value
+//       }
+//     }
+// `).then(result => {
+//     console.log("query entityLeverageFactorys")
+//     console.log(result);
+// });
 
 // type EntityTVL @entity {
 //     id: ID!
 //         timestamp: BigInt!
-//         poolAddress: Bytes!
 //         token: Bytes!
 //         amount: BigInt!
 //         value: BigInt!
 // }
-client.query(`
+// client.query(`
+//     {
+//       entityTVLs(first: 5) {
+//         id
+//         timestamp
+//         token
+//         amount
+//         value
+//       }
+//     }
+// `).then(result => {
+//     console.log("query entityTVLs")
+//     console.log(result);
+// });
+
+async function getTvls() {
+    let querystr = `
     {
-      entityTVLs(first: 5) {
-        id
-        timestamp
-        poolAddress
-        token
-        amount
-        value
-      }      
+       entityTVLs(first: 1000, orderBy: TimeStamp, orderDirection: asc) {
+            id
+            timestamp
+            token
+            amount
+            value
+      } 
+    }`
+    let result = await client.query(querystr)
+    let tvls = result.entityPoolTLVs;
+    //console.log(tvls)
+    let alltvls = new Map();
+    for(let i=0;i<tvls.length;i++) {
+        let item = tvls[i];
+        let tk = new web3.eth.Contract(tokenabi,item.Token);
+        let name = await tk.methods.symbol().call();
+        let decimal = await tk.methods.decimals().call();
+        //console.log(name,decimal)
+        let tvl = {
+            Date:getDate(item.timestamp),
+            Amount: new BN(item.amount).div(new BN(decimal)),
+            UsdValue: new BN(item.value).div(new BN(decimal).mul(PRICE_DECIMAL))
+        }
+
+        if(alltvls[name]==undefined) {
+            alltvls[name] = [];
+        }
+        alltvls[name].push(tvl);
     }
-`).then(result => {
-    console.log("query entityTVLs")
-    console.log(result);
-});
+    console.log(alltvls);
+    return alltvls
+}
 
 // type EntityInterestAPY @entity {
 //     id: ID!
@@ -127,21 +279,60 @@ client.query(`
 //     token:  Bytes
 //     apy:  BigInt
 // }
+// client.query(`
+//     {
+//       entityInterestAPYs(first: 5) {
+//         id
+//         timestamp
+//         poolAddress
+//         token
+//         apy
+//       }
+//     }
+// `).then(result => {
+//     console.log("query entityInterestAPYs")
+//     console.log(result);
+// });
 
-client.query(`
+async function getApys() {
+    let querystr = `
     {
-      entityInterestAPYs(first: 5) {
-        id
-        timestamp
-        poolAddress
-        token
-        apy
-      }      
+       entityInterestAPYs(first: 1000, orderBy: timestamp, orderDirection: asc) {
+            id
+            timestamp
+            pool
+            token
+            apy
+      } 
+    }`
+    let result = await client.query(querystr)
+    let apys = result.entityInterestAPYs;
+    console.log(apys);
+
+    let allapys = new Map();
+    for(let i=0;i<apys.length;i++) {
+        let item = apys[i];
+        let tk = new web3.eth.Contract(tokenabi,item.token);
+        let name = await tk.methods.symbol().call();
+
+        let apy = {
+            Date:getDate(item.timestamp),
+            Token: name,
+            Apy: new BN(item.apy).div(new BN("10000"))
+        }
+        if(allapys[name]==undefined) {
+            allapys[name] = [];
+        }
+        allapys[name].push(apy);
     }
-`).then(result => {
-    console.log("query entityInterestAPYs")
-    console.log(result);
-});
+
+    console.log(allapys);
+    return allapys
+}
+
+getApys();
+return;
+
 
 // type EntityTradeVol @entity {
 //     id: ID!
@@ -219,71 +410,3 @@ client.query(`
 });
 
 
-/*
-let basicUpPrices = new Map()
-let basicDownPrices = new Map()
-async function getBasicPriceData(networkname,timeBegin,timeEnd,poolAddress) {
-    let querystr = `
-    {
-      leveragedTokenPriceEntities(first:1000,orderBy:timestamp, orderDirection:desc,where: {network:\"`
-        + networkname
-        + `\",timestamp_gte:\"` + timeBegin + '\"'
-        + `,timestamp_lt:\"` + timeEnd + `\"`
-        +`,poolAddress:\"` + poolAddress +`\"`
-        +`}) {
-        poolAddress
-        uptokenprice
-        downtokenprice
-        timestamp
-      }
-    }`
-
-    console.log(querystr)
-
-    let result = await client.query(querystr)
-    let ltpes = result.leveragedTokenPriceEntities;
-  //  console.log(result)
-    for(let i=0;i<ltpes.length;i++) {
-         let item = ltpes[i]
-         basicUpPrices[item.timestamp] = item.uptokenprice.toString();
-         basicDownPrices[item.timestamp] = item.downtokenprice.toString();
-    }
-
-    return {upprices:basicUpPrices,downprices:basicDownPrices}
-}
-
-async function getprices() {
-    let res = await getBasicPriceData("rinkeby", 1620921600,1621222954, "0xb86ded607497fe38a36b26f7b5c3dfdca30ef955")
-    //console.log(res)
-    console.log("uptokenprice", res.upprices)
-    console.log("-----------------------------------------")
-    console.log("downtokenprice", res.downprices)
-}
-*/
-//getprices()
-// client.query(`
-//     {
-//       leveragedTokenPriceEntities(where: {network: "rinkeby"}) {
-//         poolAddress
-//         uptokenprice
-//         downtokenprice
-//         timestamp
-//       }
-//     }
-// `).then(result => {
-//     console.log(result);
-// });
-
-
-//exports.getBasicPriceData = getBasicPriceData
-
-// client.query(querystr).then(result => {
-//     let ltpes = result.leveragedTokenPriceEntities;
-//     for(let i=0;i<ltpes.length;i++) {
-//         let item = ltpes[i]
-//         basicUpPrices[item.timestamp] = item.uptokenprice;
-//         basicDownPrices[item.timestamp] = item.downtokenprice;
-//     }
-//
-//     return {upprices:basicDownPrices,downprices:basicDownPrices}
-// });
